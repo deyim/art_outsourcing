6350c193b17d4f09356445ffdb137733ee0b1b9c
Daeun Yim <deyim33@gmail.com>
2018-05-13 17:38:38 +0900

    socket io manipulation completed

---
+++ public/script.js
@@ -0,0 +1,67 @@
+var turnWebCam = function() {
+
+    // Normalize the various vendor prefixed versions of getUserMedia.
+    navigator.getUserMedia = (navigator.getUserMedia ||
+                              navigator.webkitGetUserMedia ||
+                              navigator.mozGetUserMedia || 
+                              navigator.msGetUserMedia);
+  
+    if (navigator.getUserMedia) {
+    // Request the camera.
+    navigator.getUserMedia(
+      // Constraints
+    {
+        video: true
+    },  
+      // Success Callback
+    function(localMediaStream) {
+        // Get a reference to the video element on the page.
+        var vid = document.getElementById('video-stream');
+
+        // Create an object URL for the video stream and use this 
+        // to set the video source.
+        vid.src = window.URL.createObjectURL(localMediaStream);
+    },
+      // Error Callback
+      function(err) {
+        // Log the error to the console.
+        console.log('The following error occurred when trying to use getUserMedia: ' + err);
+      }
+    );
+    } else {
+        alert('Sorry, your browser does not support getUserMedia');
+    }
+}
+
+var turnVideo = function(vidNum){
+  var vid = document.getElementById('video-stream');
+  vid.src = 'videos/test'+vidNum+'.mp4';
+}
+var socket = io();
+
+socket.on('connect', function(){
+    console.log('Connected to server');
+}); 
+
+socket.on('ifSync', function (data) {
+
+  console.log(data);
+
+  switch(data.ifSync) {
+    case 1:
+      console.log('turn on webcam');  //<<< not working
+      turnWebCam();
+      break;
+
+    case 0:
+      console.log('turn on video', data.vidNumber);  //<<< not working
+      turnVideo(data.vidNumber);
+      break;
+  }
+
+});
+
+socket.on('disconnect', function(){
+    console.log('disconnected');
+});
+
---
+++ server.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+var ss = require('socket.io-stream');
+const port = process.env.PORT || 3000;
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
+const publicPath = path.join(__dirname,'/public');
+app.use(express.static(publicPath));
+
+server.listen(port, ()=>{
+    console.log("Starting the server...");
+})
+
+io.on('connection', (socket)=>{
+    console.log('new user came');
+
+    socket.on('doorSync', function(msg){
+        // msg.ifSync
+        // msg.vidNumber
+        console.log(msg);
+        socket.broadcast.emit('ifSync',msg);
+    })
+
+    socket.on('webcam', ()=>{
+
+    })
+   
+    socket.on('disconnect', ()=>{
+        console.log('browser shutdown')}
+    );
+})
+
+
+// const path = require('path');
+// const express = require('express');
+// const http = require('http');
+// const socketIO = require('socket.io');
+// const port = process.env.PORT || 3000;
+
+
+// // var server = http.createServer(app);
+// var server = express.createServer();
+// var io = socketIO(server);
+// const publicPath = path.join(__dirname,'11/public');
+// app.use(express.static(publicPath));
+
+// server.get('/', (req,res)=>{
+//     // res.sendFile(path.join(__dirname+'/public/index.html'));
+//     res.render('index', {layout: false});
+// })
+
+
+// server.listen(port, ()=>{
+//     console.log("Starting the server...");
+// })
